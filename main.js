d7dc95b508fb6f1ea293d3cf36b8e74af48b74d6
WEAKNEEESSDOTWAV <weakneeess.wav@gmail.com>
2023-09-26 17:16:41 +0200

    refactor

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import {letterPicker} from "./letterPicker.js";
 
 // RANDOM LETTERS PICKER
@@ -7,14 +6,10 @@ let lettersDiv = document.getElementById("lettersDiv");
 let letterDiv = document.createElement('div');
 letterDiv.classList.add('letter');
 
-let lettersPicked = []
-
 for (let i = 0; i < 3; i++) {
 
-    lettersPicked.push(letterPicker.pickLetter("fr"));
+    letterDiv.textContent = letterPicker.pickLetter("fr");
+    lettersDiv.appendChild(letterDiv);
+    letterDiv = letterDiv.cloneNode(false);
 
 }
-
-letterDiv.textContent = lettersPicked[i];
-lettersDiv.appendChild(letterDiv);
-letterDiv = letterDiv.cloneNode(false);
